f7e0854f3a138c31a0a37cd4fcb3653f948c3c19
bq7lm <javan121007@gmail.com>
2025-05-23 20:00:02 +0300

    add postgresql

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key-here'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///locksen.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('postgresql://locksen_db_user:7omNlhR0P2FEJNzHzTSmfEmOHqUxoOh3@dpg-d0oaeq8dl3ps73ad65lg-a.oregon-postgres.render.com/locksen_db')  # 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = 'static/images'
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
